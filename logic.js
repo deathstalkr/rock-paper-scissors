5461ec6b134581a7533a24cf7bb073d747a5ee10
deathstalkr <solomon.johnson098@gmail.com>
2023-11-18 23:34:10 +0530

    added comments for better code redability

--- logic.js
+++ logic.js
@@ -1,12 +1,16 @@
 function randomNum() {
+    // get random numbers between 1 to 3
     return Math.floor(Math.random() * 3);
   }
   
   function getComputerChoice() {
     let i = randomNum();
+
+    // get computer's choice from array of choices
     // let arr = ["rock", "paper", "scissors"];
     // return arr[i];
-  
+
+    // return computer's choice using switch statement
     switch (i) {
       case 0:
         return "rock";
@@ -22,7 +26,8 @@ function randomNum() {
   
   //   function playRound(playerSelection, computerSelection) {
   //     const player = playerSelection.toLowerCase();
-  
+
+         // using if else for all possible combination
   //     if (player == "rock" && computerSelection == "paper") {
   //       return `You lose! Paper beats Rock`;
   //     } else if (player == "paper" && computerSelection == "rock") {
@@ -48,7 +53,8 @@ function randomNum() {
   
   function playRound(playerSelection, computerSelection) {
     const player = playerSelection.toLowerCase();
-  
+
+    // modified if - else statement by checking multiple combinations at once 
     if (player === computerSelection) {
       return `Draw! Taking re-match`;
     } else if (player == "rock" && computerSelection == "paper" || player == "scissors" && computerSelection == "rock" || player == "paper" && computerSelection == "scissors") {
@@ -65,6 +71,8 @@ function randomNum() {
     let computerSelection = '';
     let count = 0;
     alert(`You Vs Computer\n5 match series\n0 - 0`)
+    
+    // for 5 match series calling the playRound function 5 times using loop
     for (let i = 0; i < 5; i++) {
   
       playerSelection = prompt(`Please choose any one:\nrock, paper or scissors`, '');
@@ -73,13 +81,16 @@ function randomNum() {
       let match = playRound(playerSelection, computerSelection)
       alert(match);
       console.log(match);
-  
+      
+      // Updating player score and also calling for a re-match if draw or invalid input is obtained.
       if (match.includes("win!")) {
         count += 1;
       } else if (match.includes("Please enter a valid option") || match.includes("Draw!")) {
         i--;
       }
     }
+
+    // Showing the series results 
     if (count <= Math.floor(5 / 2)) {
       console.log(`You lost the series against the computer!! ${count} - ${5-count}`)
       return alert(`You lost the series against the computer!!\n${count} - ${5-count}`)
