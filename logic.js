678e24a613a39a57a91356ceabe2ee5c4d32396d
deathstalkr <solomon.johnson098@gmail.com>
2023-11-20 11:09:47 +0530

    updated comments to tell 'why' rather than 'how'

--- logic.js
+++ logic.js
@@ -1,16 +1,11 @@
 function randomNum() {
-    // get random numbers between 1 to 3
+    // return a random number between 1 and 3 to choose an option for the computer
     return Math.floor(Math.random() * 3);
   }
   
   function getComputerChoice() {
+    // return computer's choice based on the random number obtained above
     let i = randomNum();
-
-    // get computer's choice from array of choices
-    // let arr = ["rock", "paper", "scissors"];
-    // return arr[i];
-
-    // return computer's choice using switch statement
     switch (i) {
       case 0:
         return "rock";
@@ -24,42 +19,19 @@ function randomNum() {
     }
   }
   
-  //   function playRound(playerSelection, computerSelection) {
-  //     const player = playerSelection.toLowerCase();
-
-         // using if else for all possible combination
-  //     if (player == "rock" && computerSelection == "paper") {
-  //       return `You lose! Paper beats Rock`;
-  //     } else if (player == "paper" && computerSelection == "rock") {
-  //       return `You Win! Paper beats Rock`;
-  //     } else if (player == "rock" && computerSelection == "scissors") {
-  //       return `You Win! Rock beats Scissors`;
-  //     } else if (player == "scissors" && computerSelection == "rock") {
-  //       return `You loose! Rock beats Scissors`;
-  //     } else if (player == "paper" && computerSelection == "scissors") {
-  //       return `You Loose! Scissors beats Paper`;
-  //     } else if (player == "scissors" && computerSelection == "paper") {
-  //       return `You Win! Scissors beats Paper`;
-  //     } else if (player == "rock" && computerSelection == "rock") {
-  //       return `Draw! play again`;
-  //     } else if (player == "paper" && computerSelection == "paper") {
-  //       return `Draw! play again`;
-  //     } else if (player == "scissors" && computerSelection == "scissors") {
-  //       return `Draw! play again`;
-  //     } else {
-  //       return `Please enter a valid option`;
-  //     }
-  //   }
-  
   function playRound(playerSelection, computerSelection) {
     const player = playerSelection.toLowerCase();
 
-    // modified if - else statement by checking multiple combinations at once 
+    // return results based on the choices of both player and computer
     if (player === computerSelection) {
       return `Draw! Taking re-match`;
-    } else if (player == "rock" && computerSelection == "paper" || player == "scissors" && computerSelection == "rock" || player == "paper" && computerSelection == "scissors") {
+    } else if (player == "rock" && computerSelection == "paper" ||
+    player == "scissors" && computerSelection == "rock" ||
+    player == "paper" && computerSelection == "scissors") {
       return `You loose! ${computerSelection} beats ${player}`;
-    } else if (player == "paper" && computerSelection == "rock" || player == "rock" && computerSelection == "scissors" || player == "scissors" && computerSelection == "paper") {
+    } else if (player == "paper" && computerSelection == "rock" ||
+    player == "rock" && computerSelection == "scissors" ||
+    player == "scissors" && computerSelection == "paper") {
       return `You win! ${player} beats ${computerSelection}`;
     } else {
       return `Please enter a valid option. Taking re-match`;
@@ -72,7 +44,7 @@ function randomNum() {
     let count = 0;
     alert(`You Vs Computer\n5 match series\n0 - 0`)
     
-    // for 5 match series calling the playRound function 5 times using loop
+    // create a 5 match series
     for (let i = 0; i < 5; i++) {
   
       playerSelection = prompt(`Please choose any one:\nrock, paper or scissors`, '');
@@ -82,7 +54,7 @@ function randomNum() {
       alert(match);
       console.log(match);
       
-      // Updating player score and also calling for a re-match if draw or invalid input is obtained.
+      // update player score and do a re-match if draw or invalid input is obtained.
       if (match.includes("win!")) {
         count += 1;
       } else if (match.includes("Please enter a valid option") || match.includes("Draw!")) {
@@ -90,13 +62,16 @@ function randomNum() {
       }
     }
 
-    // Showing the series results 
+    // return the series results 
     if (count <= Math.floor(5 / 2)) {
-      console.log(`You lost the series against the computer!! ${count} - ${5-count}`)
-      return alert(`You lost the series against the computer!!\n${count} - ${5-count}`)
+      console.log(`You lost the series against the computer!! 
+      ${count} - ${5-count}`)
+      return alert(`You lost the series against the computer!!\n
+      ${count} - ${5-count}`)
     } else {
       console.log(`You won the series against the computer!! Congratulations. ${count} - ${5-count}`)
-      return alert(`You won the series against the computer!! Congratulations.\n${count} - ${5-count}`)
+      return alert(`You won the series against the computer!! Congratulations.\n
+      ${count} - ${5-count}`)
     }
   }
   
